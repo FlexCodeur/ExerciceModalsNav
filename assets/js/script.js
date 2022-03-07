3200ef53d4818e8b7bd73483444d425a64e75e1c
FlexCodeur <mehdi.rezgane@campusdulac.com>
2022-03-07 20:36:58 +0100

    Exercice dropdown menu JS Beta 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 const openModals = document.getElementsByClassName("openModal");
 const modals = document.getElementsByClassName("secondNav");
 
-for( let i = 0 ; i < openModals.length ; i++){
-    openModals[i].addEventListener("mouseenter", function(){
-        modals[i].classList.remove('displayNone');
-    });
-    openModals[i].addEventListener("mouseout", function(){
-        modals[i].classList.add("displayNone");
-    })
+for (let i = 0; i < openModals.length; i++) {
+  openModals[i].addEventListener("mouseenter", () => {
+    modals[i].classList.remove("displayNone");
+  });
+  window.addEventListener("mousemove", (e) => {
+    if (e.offsetY > 105) {
+      modals[i].classList.add("displayNone");
+    }
+  });
 }
